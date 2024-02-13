1aeb5c4b3951034f8eb7bbafec818b9faf925958
Maria A. Valencia <fafevalo99@gmail.com>
2024-02-13 09:53:20 -0500

    firt commit

---
+++ src/partials/Hero.tsx
@@ -0,0 +1,64 @@
+import {
+  GradientText,
+  HeroAvatar,
+  HeroSocial,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Hero = () => (
+  <Section>
+    <HeroAvatar
+      title={
+        <>
+          Hola, soy <GradientText>faiver</GradientText> 👋
+        </>
+      }
+      description={
+        <>
+          Ingeniero civil graduado de la universidad de Medellin, con
+          experiencia en el campo de la construcción de obras civiles. He
+          participado en proyectos de gran escala a nivel nacional como se
+          indica mas adelante.{' '}
+        </>
+      }
+      avatar={
+        <img
+          className="h-80 w-64"
+          src="/assets/images/avatar.svg"
+          alt="Avatar image"
+          loading="lazy"
+        />
+      }
+      socialButtons={
+        <>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/twitter-icon.png"
+              alt="Twitter icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/facebook-icon.png"
+              alt="Facebook icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/linkedin-icon.png"
+              alt="Linkedin icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/youtube-icon.png"
+              alt="Youtube icon"
+            />
+          </a>
+        </>
+      }
+    />
+  </Section>
+);
+
+export { Hero };
